080b76c4f31c6289b73bb5309fd93c25565c541f
Gunnar <dunkcrunk.com>
2021-08-10 23:11:01 -0600

    Got user story 8 done with the link for invoice that do beat the threshold for the bulk discounts and has a link to that specific bulk discounts show page.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_one :merchant, through: :item
+  has_many :bulk_discounts, through: :merchant
 
   enum status: [:pending, :packaged, :shipped]
 
@@ -10,5 +12,16 @@ class InvoiceItem < ApplicationRecord
 
   def self.total_revenue
     sum("quantity * unit_price")
+  end
+
+  def has_discount?
+    bulk_discounts.where('bulk_discounts.quantity_threshold <= ?', quantity)
+                  .size >= 1
+  end
+
+  def find_discount
+    bulk_discounts.where('bulk_discounts.quantity_threshold <= ?', quantity)
+                  .order(percentage_discount: :desc)
+                  .first
   end
 end
